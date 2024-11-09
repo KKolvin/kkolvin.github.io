a7b26030952467bdfc030af2fe5c6ba81023a30d
Kolvin <kewenliu@bu.edu>
2024-11-09 01:12:41 -0500

    add birdy

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+    return (
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <a href="https://github.com/KKolvin/Static-Web-Birdy/docs" target="_blank" rel="noopener noreferrer">
+          View Birdy
+        </a>
+      </div>
+    );
+  }
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Renders the <App /> component into the <div id="root"></div> in index.html
+ReactDOM.render(<App />, document.getElementById('root'));
