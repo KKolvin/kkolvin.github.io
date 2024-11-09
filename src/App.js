036266f20154dce1886ed622406baec9dce7acea
Kolvin <kewenliu@bu.edu>
2024-11-09 04:09:24 -0500

    add something

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React from 'react';
 function App() {
     return (
       <div>
-        <h1>Welcome to My Portfolio</h1>
-        <a href="https://kkolvin.github.io/Static-Web-Birdy/index.html" target="_blank" rel="noopener noreferrer">
-          View Birdy
-        </a>
+        <div class="profile-holder">
+          <img class="profile-pic" src="../assets/img/me.png"></img>
+          <h1 class="profile-heading">KOLVIN LIU</h1>
+        </div>
+
+        <div>
+          <a href="https://kkolvin.github.io/Static-Web-Birdy/index.html" target="_blank" rel="noopener noreferrer">
+          View Birdy</a>
+        </div>
       </div>
     );
   }
